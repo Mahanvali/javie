298c20979e8cd4047ee0e507ff1ded8cb0b1a917
Mahanvali <139391502+Mahanvali@users.noreply.github.com>
2024-08-29 06:19:25 +0330

    Update LinksCommand.java

--- src/main/java/com/mycompany/app/Commands/misc/LinksCommand.java
+++ src/main/java/com/mycompany/app/Commands/misc/LinksCommand.java
@@ -9,19 +9,16 @@ import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEve
 public class LinksCommand implements CommandImplementation {
     @Override
     public void execute(SlashCommandInteractionEvent event){
-        
+
         EmbedBuilder embed = new EmbedBuilder();
-        embed.setTitle("Check out all of these links!");
         embed.setColor(Global.CUSTOMPURPLE);
         embed.setImage("https://cdn.discordapp.com/attachments/1270505313627406368/1277622963834847383/IMG_7317.webp?ex=66d0799b&is=66cf281b&hm=49137c68371e9f0d09e4135d56e3a3f16819e008157897519c50199f691e59c4&");
-        embed.setDescription(
-            Global.yukariHEART + " **[Twitter](https://x.com/yukarivt)**\n"
-            + Global.yukari4K + " **[Twitch](https://twitch.tv/yukarivt)**\n"
-            + Global.yukariBONK + " **[Discord](https://discord.gg/yukarii)**\n"
-            + Global.yukariSEARCH + " **[Youtube](https://www.youtube.com/@yukariVODs)**\n"
-            + Global.yukariOHOH + " **[Ko-Fi](https://ko-fi.com/yukarivt)**\n"
-            + Global.yukariYES + " **[TikTok](https://www.tiktok.com/@yukarivt)**\n"
-            + Global.yukariEVIL + " **[Donations](https://streamelements.com/yukarivt/tip)**");
+        embed.addField(Global.yukari4K + " Twitter", "**[here](https://x.com/yukarivt)**", true);
+        embed.addField(Global.yukariNOTED + " Twitch", "**[here](https://twitch.tv/yukarivt)**", true);
+        embed.addField(Global.yukariWAVE + " Discord", "**[here](https://discord.gg/yukarii)**", true);
+        embed.addField(Global.yukariYES + " Youtube", "**[here](https://www.youtube.com/@yukariVODs)**", true);
+        embed.addField(Global.yukariHEART + " Ko-Fi", "**[here](https://ko-fi.com/yukarivt)**", true);
+        embed.addField(Global.yukariSMH + " TikTok", "**[here](https://www.tiktok.com/@yukarivt)**", true);
 
         if(event.getChannel().asTextChannel().getId().equals(Global.botCommandsChannelId)){
             event.replyEmbeds(embed.build()).queue();
